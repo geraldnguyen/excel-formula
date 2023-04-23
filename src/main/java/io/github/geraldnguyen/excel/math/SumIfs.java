26ddec1887707414291f222071e6d835ffbe2d11
Gerald Nguyen <ngthehuy@gmail.com>
2023-04-23 12:18:43 +0800

    CountIfs - supporting Long, Double, BigDecimal

--- src/main/java/io/github/geraldnguyen/excel/math/SumIfs.java
+++ src/main/java/io/github/geraldnguyen/excel/math/SumIfs.java
@@ -3,12 +3,34 @@ package io.github.geraldnguyen.excel.math;
 import io.github.geraldnguyen.excel.BaseIfs;
 import io.github.geraldnguyen.excel.RangeCriteria;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 // https://support.microsoft.com/en-us/office/sumifs-function-c9e748f5-7ea7-455d-9406-611cebce642b
 public class SumIfs extends BaseIfs {
-    protected Integer sumIfs(List<Integer> range, RangeCriteria<?>... rangeCriteria) {
+    protected <T extends Number> T sumIfs(List<T> range, RangeCriteria<?>... rangeCriteria) {
         var elems = ifs(range, rangeCriteria);
-        return elems.stream().reduce(Integer::sum).orElse(null);
+        return elems.stream().reduce(SumIfs::sum).orElse(null);
     }
+
+    public static <T extends Number> T sum(T n1, T n2) {
+        if (n1 instanceof Integer) {
+            return (T) Integer.valueOf(Integer.sum(n1.intValue(), n2.intValue()));
+        }
+        if (n1 instanceof Long) {
+            return (T) Long.valueOf(Long.sum(n1.longValue(), n2.longValue()));
+        }
+        if (n1 instanceof Double) {
+            return (T) Double.valueOf(Double.sum(n1.doubleValue(), n2.doubleValue()));
+        }
+        if (n1 instanceof Double) {
+            return (T) Double.valueOf(Double.sum(n1.doubleValue(), n2.doubleValue()));
+        }
+        if (n1 instanceof BigDecimal) {
+            return (T) ((BigDecimal) n1).add((BigDecimal) n2);
+        }
+
+        throw new IllegalArgumentException("Type not supported: " + n1.getClass());
+    }
+
 }
--- src/test/java/io/github/geraldnguyen/excel/math/SumIfsTest.java
+++ src/test/java/io/github/geraldnguyen/excel/math/SumIfsTest.java
@@ -3,6 +3,7 @@ package io.github.geraldnguyen.excel.math;
 import io.github.geraldnguyen.excel.RangeCriteria;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,5 +28,46 @@ class SumIfsTest {
                 new RangeCriteria<>(salespersons, isEqual("Tom"))
         ));
 
+    }
+
+    @Test
+    void sumIfsLong() {
+        var quantities = List.of(5L, 4L, 15L, 3L, 22L, 12L, 10L, 33L);
+        var products = List.of("Apples", "Apples", "Artichokes", "Artichokes",
+                "Bananas", "Bananas", "Carrots", "Carrots");
+        var salespersons = List.of("Tom", "Sarah", "Tom", "Sarah", "Tom", "Sarah", "Tom", "Sarah");
+
+        assertEquals(20L, (new SumIfs()).sumIfs(quantities,
+                new RangeCriteria<>(products, startWith("A")),
+                new RangeCriteria<>(salespersons, isEqual("Tom"))
+        ));
+    }
+
+    @Test
+    void sumIfsDouble() {
+        var quantities = List.of(5.1, 4.2, 15.3, 3.4, 22.5, 12.6, 10.7, 33.8);
+        var products = List.of("Apples", "Apples", "Artichokes", "Artichokes",
+                "Bananas", "Bananas", "Carrots", "Carrots");
+        var salespersons = List.of("Tom", "Sarah", "Tom", "Sarah", "Tom", "Sarah", "Tom", "Sarah");
+
+        assertEquals(20.4, (new SumIfs()).sumIfs(quantities,
+                new RangeCriteria<>(products, startWith("A")),
+                new RangeCriteria<>(salespersons, isEqual("Tom"))
+        ));
+    }
+
+    @Test
+    void sumIfsBigDecimal() {
+        var quantities = List.of(BigDecimal.valueOf(5.1), BigDecimal.valueOf(4.2), BigDecimal.valueOf(15.3),
+                BigDecimal.valueOf(3.4), BigDecimal.valueOf(22.5), BigDecimal.valueOf(12.6),
+                BigDecimal.valueOf(10.7), BigDecimal.valueOf(33.8));
+        var products = List.of("Apples", "Apples", "Artichokes", "Artichokes",
+                "Bananas", "Bananas", "Carrots", "Carrots");
+        var salespersons = List.of("Tom", "Sarah", "Tom", "Sarah", "Tom", "Sarah", "Tom", "Sarah");
+
+        assertEquals(BigDecimal.valueOf(20.4), (new SumIfs()).sumIfs(quantities,
+                new RangeCriteria<>(products, startWith("A")),
+                new RangeCriteria<>(salespersons, isEqual("Tom"))
+        ));
     }
 }
